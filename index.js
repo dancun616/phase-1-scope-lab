814de4d708e24bd8e9884e0952f683917a6cba1f
dancun616 <dancun616@gmail.com>
2022-11-28 15:21:44 +0300

    completed message

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName='bob';
+const leastFavoriteCustomer= 'Jakes';
+function upperCaseCustomerName() {
+    customerName= (customerName.toUpperCase());
+    return(customerName);
+}
+function setBestCustomer() {
+    bestCustomer= "not bob";
+}
+console.log(setBestCustomer);
+function overwriteBestCustomer(awesomeCustomer) {
+    bestCustomer= awesomeCustomer;
+}
+console.log(overwriteBestCustomer());
+function changeLeastFavoriteCustomer() {
+    return(leastFavoriteCustomer='Gad');
+}
+console.log(changeLeastFavoriteCustomer());
+console.log(leastFavoriteCustomer);
